0a5cb3dcfb608d6ad27bf175358e673b86c5a29e
Chinmay P Dev <chinmaysjr26@gmail.com>
2025-03-20 22:56:09 +0530

    v0.3.1

--- src/components/maintenance.tsx
+++ src/components/maintenance.tsx
@@ -4,13 +4,17 @@ import { playfairDisplay } from "../ui/ui";
 
 export const Maintenance = () => (
 	<div
-		className={`grid place-items-center grid-rows-[20px_1fr_20px] w-full min-h-1/2 p-8 pb-20 gap-16 sm:p-20 ${playfairDisplay.className}`}
+		className={`grid place-items-center bg-black grid-rows-[20px_1fr_20px] w-full min-h-1/2 p-8 pb-20 gap-16 sm:p-20 ${playfairDisplay.className}`}
 	>
 		<div className="flex flex-col gap-[32px] row-start-2 justify-center items-center">
 			<Image src="/small-white.png" alt="HustleX logo" width={72} height={72} priority />
 			<ul className="text-xl text-center sm:text-left not-last:mb-2">
-				<li className="letter-spacing-[0.1em]">Welcome to the HustleX experience.</li>
-				<li className="letter-spacing-[0.1em]">The website is under maintenance.</li>
+				<li className="letter-spacing-[0.1em]  text-white">
+					Welcome to the HustleX experience.
+				</li>
+				<li className="letter-spacing-[0.1em]  text-white">
+					The website is under maintenance.
+				</li>
 			</ul>
 		</div>
 	</div>
