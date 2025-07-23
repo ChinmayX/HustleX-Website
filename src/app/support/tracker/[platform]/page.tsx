cb20379202449278bd8059e393689f47d0984009
Siddharth Praveen Bharadwaj <srsfool@gmail.com>
2025-07-23 12:30:40 +0530

    Updated tracker docs

--- src/app/support/tracker/[platform]/page.tsx
+++ src/app/support/tracker/[platform]/page.tsx
@@ -4,38 +4,16 @@ import React from "react";
 
 import AndroidTracker from "@/markdown/platform/android.mdx";
 import AppleTracker from "@/markdown/platform/ios.mdx";
-
-import GoogleFit from "@/markdown/trackers/google-fit.mdx";
-import SamsungHealth from "@/markdown/trackers/samsung-health.mdx";
-import Fitbit from "@/markdown/trackers/fitbit.mdx";
-import MyFitnessPal from "@/markdown/trackers/myfitnesspal.mdx";
-import Garmin from "@/markdown/trackers/garmin.mdx";
-import NikeRunClub from "@/markdown/trackers/nike-run-club.mdx";
-import Coros from "@/markdown/trackers/coros.mdx";
-
 import { Container } from "@/ui/markdown";
-import { useRouter } from "next/navigation";
-
-const trackerComponentMap: Record<string, React.ComponentType> = {
-	"google-fit": GoogleFit,
-	"samsung-health": SamsungHealth,
-	"fitbit": Fitbit,
-	"myfitnesspal": MyFitnessPal,
-	"garmin": Garmin,
-	"nike-run-club": NikeRunClub,
-	"coros": Coros,
-};
+import dynamic from "next/dynamic";
 
 const TrackerPlatform = (props: { params: Promise<{ platform: string }> }) => {
-	const router = useRouter();
-
 	const { platform } = React.use(props.params);
-	const TrackerComponent = trackerComponentMap[platform.toLowerCase()];
+	const TrackerComponent = dynamic(
+		() => import(`@/markdown/trackers/${platform.toLowerCase()}.mdx`),
+		{ ssr: false, loading: () => <div>Loading...</div> },
+	);
 
-	if (!TrackerComponent && platform !== "ios" && platform !== "android") {
-		router.back();
-		return null;
-	}
 	return (
 		<Container>
 			{platform === "ios" ? (
--- src/ui/fonts.tsx
+++ src/ui/fonts.tsx
@@ -8,7 +8,6 @@ const playfairDisplay = Playfair_Display({
 });
 const ibmPlexMono = IBM_Plex_Mono({ subsets: ["latin"], weight: ["400", "700"] });
 const outfit = Outfit({ subsets: ["latin"], weight: ["400", "700"] });
-const basement = localFont({ src: "../../public/fonts/Basement.woff2", weight: "400" });
 const instrumentSerif = Instrument_Serif({ subsets: ["latin"], weight: ["400"] });
 
-export { playfairDisplay, ibmPlexMono, outfit, basement, instrumentSerif };
+export { playfairDisplay, ibmPlexMono, outfit, instrumentSerif };
